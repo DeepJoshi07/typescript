7e50d72b69076e0d3ce9e119afea483755bf3a4a
Deep joshi <joshideep972@gmail.com>
2026-04-23 16:07:32 +0530

    typescript lession 1

---
+++ 1_lession/build/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let username = "Deep";
+console.log(username);
+let a = 12;
+// let b:string = '6'
+let b = 6;
+let c = 2;
+console.log(a / b); // gives error because of type infuring
+console.log(a / c);
+//# sourceMappingURL=main.js.map
