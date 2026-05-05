519c1d886a3edfd7d3411d69c94d36c39a6e480d
Deep joshi <joshideep972@gmail.com>
2026-05-05 09:36:28 +0530

    lession 8

---
+++ 8_lession/src/main.ts
@@ -0,0 +1,152 @@
+// Generics
+// <T>
+
+const echo = <T>(args: T):T => args;
+
+const isObj = <T>(arg: T):boolean =>{
+    return (typeof arg === "object" && Array.isArray(arg) && arg !== null);
+}
+
+console.log(isObj(true)) // false
+console.log(isObj('john')) // false
+console.log(isObj([1,2,3])) // false
+console.log(isObj({name:'john'})) // true
+console.log(isObj(null)) // false
+
+const isTrue = <T>(arg: T): {arg: T, is:boolean} =>{
+    if(Array.isArray(arg) && !arg.length){
+        return {arg,is:false};
+    }
+
+    if(isObj(arg) && Object.keys(arg as keyof T).length){
+        return {arg,is:false}
+    }
+
+    return {arg,is: !!arg}
+}
+
+console.log("------------------");
+console.log(isTrue(false))
+console.log(isTrue(0))
+console.log(isTrue(true))
+console.log(isTrue(1))
+console.log(isTrue('deep'))
+console.log(isTrue(''))
+console.log(isTrue(null))
+console.log(isTrue(undefined))
+console.log(isTrue({}))
+console.log(isTrue({name:'deep'}))
+console.log(isTrue([]))
+console.log(isTrue([1,2,3]))
+console.log(isTrue(NaN))
+console.log(isTrue(-0))
+
+interface BoolCheck<T>{
+    value:T,
+    is:boolean
+}
+
+const checkBoolValue = <T>(arg: T): BoolCheck<T> =>{
+    if(Array.isArray(arg) && !arg.length){
+        return {value:arg,is:false};
+    }
+
+    if(isObj(arg) && Object.keys(arg as keyof T).length){
+        return {value:arg,is:false}
+    }
+
+    return {value:arg,is: !!arg}
+}
+
+interface HasID {
+    id:number
+}
+
+const processUser = <T extends HasID>(user : T):T=>{
+    return user;
+}
+
+console.log(processUser({id:1,name:'dave'}));
+// console.log(processUser({name:'deep'})) // error
+// our object must have an id because T extends HasID
+
+const getUserProperty = <T extends HasID,K extends keyof T>(users:T[],key:K):T[K][]=>{
+return users.map(user => user[key]);
+}
+
+const usersArray = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+]
+
+console.log(getUserProperty(usersArray,"email"));
+console.log(getUserProperty(usersArray,"username"));
+
+class StateObject<T>{
+    private data:T
+    constructor(value:T){
+        this.data = value;
+    }
+
+    get state(): T{
+        return this.data;
+    }
+
+    set state(value:T){
+        this.data = value
+    }
+}
+
+const store = new StateObject("john");
+console.log(store.state);
+store.state = "deep";
+// store.state = 12 // error
+// because we initialized it with string data
+
+const myStore = new StateObject<(string|number|boolean)[]>([15]);
+myStore.state = (['deep',23,true])
+console.log(myStore.state)
