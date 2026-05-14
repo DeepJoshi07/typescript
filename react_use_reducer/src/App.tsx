b262bd9059282afe9e3070924f6c6240fc27fed5
Deep Joshi <joshideep972@gmail.com>
2026-05-14 09:18:27 +0530

    use reducer hook with typescript

---
+++ react_use_reducer/src/App.tsx
@@ -0,0 +1,12 @@
+import Counter from "./components/Counter"
+
+
+function App() {
+  return (
+    <>
+      <Counter>{(num:number) =><>Current Count :{num}</>}</Counter>
+    </>
+  )
+}
+
+export default App
