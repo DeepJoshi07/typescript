f23284b1893dab0d8ce592bcac816aba9825734a
Deep Joshi <joshideep972@gmail.com>
2026-05-23 11:57:37 +0530

    adding cart, cart-line-item and optimization of code

--- react_typescript_project/src/components/Product.tsx
+++ react_typescript_project/src/components/Product.tsx
@@ -1,24 +1,62 @@
-import { type ProductType } from "../context/ProductsProvider"
-import { type ReducerActionType, ReducerAction } from "../context/CartProvider"
-import type { ReactElement } from "react"
-
+import { type ProductType } from "../context/ProductsProvider";
+import {
+  type ReducerActionType,
+  type ReducerAction,
+} from "../context/CartProvider";
+import { memo, type ReactElement } from "react";
 
 type PropsType = {
-    product: ProductType,
-    dispatch:React.ActionDispatch<[action: ReducerAction]>,
-    REDUCER_ACTIONS: ReducerActionType,
-    inCart:boolean
-}
-const Product = ({product,dispatch,REDUCER_ACTIONS,inCart}:ProductType):ReactElement => {
+  product: ProductType;
+  dispatch: React.ActionDispatch<[action: ReducerAction]>;
+  REDUCER_ACTIONS: ReducerActionType;
+  inCart: boolean;
+};
+const Product = ({
+  product,
+  dispatch,
+  REDUCER_ACTIONS,
+  inCart,
+}: PropsType): ReactElement => {
+  const img: string = new URL(`../images/${product.sku}.jpg`, import.meta.url)
+    .href;
 
-    const img:string = new URL(`../images/${product.sku}.jpg`,import.meta.url).href;
+  const onAddToCart = () =>
+    dispatch({ type: REDUCER_ACTIONS.ADD, payload: { ...product, qty: 1 } });
 
-    const onAddToCart = () => {
-        
-    }
+  const itemInCart = inCart ? "-> Item in Cart : true" : null;
+
+  const content = (
+    <article className="product">
+      <h3>{product.name}</h3>
+      <img src={img} alt={product.name} className="product__img" />
+      <p>
+        {new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(product.price)}{" "}
+        {itemInCart}
+      </p>
+      <button onClick={onAddToCart}>Add to Cart</button>
+    </article>
+  );
+
+  return content;
+};
+
+function areProductsEqual(
+  { product: prevProduct, inCart: prevInCart }: PropsType,
+  { product: nextProduct, inCart: nextInCart }: PropsType,
+) {
   return (
-    <div>Product</div>
-  )
+    Object.keys(prevProduct).every((key) => {
+      return (
+        prevProduct[key as keyof ProductType] ===
+        nextProduct[key as keyof ProductType]
+      );
+    }) && prevInCart === nextInCart
+  );
 }
 
-export default Product
+const MemoizedProduct = memo<typeof Product>(Product, areProductsEqual);
+
+export default MemoizedProduct;
--- react_typescript_project/src/main.tsx
+++ react_typescript_project/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { CartProvider } from "./context/CartProvider.tsx";
+import { ProductsProvider } from "./context/ProductsProvider.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <ProductsProvider>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </ProductsProvider>,
+);
